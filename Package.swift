dff2edc86f1fb121f180aaa5d122844596ee1098
Przemyslaw Pawluk <ppawluk@georgebrown.ca>
2025-01-06 18:22:18 -0500

    Update Package.swift
    Assignment

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "Assignment",
-            targets: ["Calculator"]),
+            targets: ["Assignment"]),
     ],
     dependencies: [],
     targets: [
